82f1ed52fd40a4dc039fb181673b8bd2a1a4fc72
(Student  C00301332) Pavel Dobias <C00301332@setu.ie>
2025-03-18 11:52:48 +0000

    starting Advanced CSS task

---
+++ svelte-app/src/lib/components/Header.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { Navigation } from "$lib";
+</script>
+
+
+<header>
+    <!-- Logo -->
+    <div class = "logo">
+        <img src = "/logo.png" alt = "Website Logo">
+    </div>
+
+    <!-- Navigation -->
+    <Navigation />
+</header>
+
+<style>
+     header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: var(--primary-color);
+        padding: 1rem;
+    }
+
+    .logo {
+        display: flex;
+        align-items: left;
+    }
+
+    .logo img {
+        width: 50px;
+        margin-right: 0.625rem;
+    }
+</style>
---
+++ svelte-app/src/lib/components/Navigation.svelte
@@ -0,0 +1,36 @@
+<nav>
+    <!--
+    <ul>
+        <li><a href="https://github.com/PavelDobias2002"><img src="/cvredbrightsmaller.png" alt="CVicon" /></a></li>
+        <li><a href="https://github.com/PavelDobias2002"><img src="/gitredbrightsmall.png" alt="GitHubIcon" /></a></li>
+        <li><a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://ie.linkedin.com/in/pavel-dobias-789060292&ved=2ahUKEwjy_8-tl4KMAxWHUUEAHQNCAtoQFnoECBYQAQ&usg=AOvVaw06-nsywOiiTPqT1gHErbOe"><img src="/linkedinredbrightsmall.png" alt="LinkedInIcon" /></a></li>
+    </ul>
+    <ul class="homepage">
+        <li><a href="/"> Home </a></li>
+    </ul>
+    -->
+</nav>
+
+<style>
+    nav ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        justify-content: right;
+    }
+
+    
+    
+    nav ul li {
+        margin: 0 15px;
+    }
+    
+    nav ul li a {
+        font-family: 'Pixelify Sans';
+        font-size: large;
+        text-decoration: none;
+        color: white;
+        font-weight: bold;
+    }
+</style>
